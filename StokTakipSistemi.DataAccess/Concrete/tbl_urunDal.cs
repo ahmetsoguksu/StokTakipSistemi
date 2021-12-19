b3a34297c144714cc4c76d6ee9c911bc67055309
Ahmet Soğuksu <dzcahmet@gmail.com>
2021-12-19 15:56:21 +0300

    Verilere erişim başarılı
    Veri erişim, Entities ve Business katmanlarında yazılanlar başarıyla çalıştı ve deneme amaçlı Form1'e eklediğim datagridview'de ürünler tablosunu görebildim.
    
    Notion: (0.001)
    https://www.notion.so/0-001-Github-19809d50f4c7475686faa5db79d0db71

---
+++ StokTakipSistemi.DataAccess/Concrete/tbl_urunDal.cs
@@ -0,0 +1,43 @@
+﻿using StokTakipSistemi.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.DataAccess.Concrete
+{
+    public class tbl_urunDal
+    {
+        public List<tbl_urun> GetAll()
+        {
+            using (StokTakipSistemiContext context = new StokTakipSistemiContext())
+            {
+                return context.Urunler.ToList();
+            }
+        }
+        public tbl_urun Get(int id)
+        {
+            using (StokTakipSistemiContext context = new StokTakipSistemiContext())
+            {
+                return context.Urunler.SingleOrDefault(p => p.urun_id == id);
+            }
+        }
+        public void Add(tbl_urun urun)
+        {
+            using (StokTakipSistemiContext context = new StokTakipSistemiContext())
+            {
+                context.Urunler.Add(urun);
+                context.SaveChanges();
+            }
+        }
+        public void Update(tbl_urun urun)
+        {
+
+        }
+        public void Delete(tbl_urun urun)
+        {
+
+        }
+    }
+}
---
+++ StokTakipSistemi.Entities/Concrete/tbl_urun.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.Entities.Concrete
+{
+    public class tbl_urun
+    {
+        [Key]
+        public int urun_id { get; set; }
+        public string urun_ad { get; set; }
+        public int kategori_id { get; set; }
+        public string urun_birim { get; set; }
+        public int urun_stok_miktari { get; set; }
+        public decimal urun_kdv_orani { get; set; }
+        public decimal urun_son_alis_fiyati { get; set; }
+        public decimal urun_son_satis_fiyati { get; set; }
+    }
+}
